ce95fb6c861caf6e1ca32da96ecc5f6a38a4d2ca
Liridon Loku <liridonloku@gmail.com>
2021-09-14 11:34:33 +0200

    Create helper modules

---
+++ src/projects.js
@@ -0,0 +1,12 @@
+const projects = []; //project container
+
+//project factory function
+
+
+//add function
+
+
+//remove function
+
+
+//project getter
---
+++ src/tasks.js
@@ -0,0 +1,15 @@
+const tasks = []; //task container
+
+//task factory function
+
+
+//add function
+
+
+//remove function
+
+
+//mark as done function
+
+
+//task getter
