708073d0e4375620f351fdb0a6953ab88ac8fcc8
Liridon Loku <liridonloku@gmail.com>
2021-09-17 12:50:50 +0200

    Link style.css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import {addTask, editTask, removeTask, markDone, getTasks} from './tasks.js';
 import {addProject, removeProject, getProjects, editProject } from './projects.js';
 import {defaultDisplay, todayDisplay, projectDisplay, weekDisplay} from './display.js';
+import './style.css';
 
 addTask('Task1', 'Desc', 'default', '2021-01-01', '1');
 addTask('Task2', 'Desc', 'default', '2021-04-02', '1');
