984e7809cc8476e8195e0dd10226494564b25310
Liridon Loku <liridonloku@gmail.com>
2021-09-20 20:52:10 +0200

    Add styling for task container

--- src/index.js
+++ src/index.js
@@ -10,4 +10,3 @@ addTask('Task2', 'Desc', 'default', '2021-04-02', '1');
 addTask('Task3', 'Desc', 'default', '2021-02-01', '1');
 addTask('Task4', 'Desc', 'default2', '2021-01-03', '1');
 addTask('Task5', 'Desc', 'default2', '2021-09-15', '1');
-
