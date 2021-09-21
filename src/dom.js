e35b298f014ca1ddbbb4019b1d23b27bd60ac03f
Liridon Loku <liridonloku@gmail.com>
2021-09-21 13:52:00 +0200

    Add loadTasks function to dom.js

--- src/dom.js
+++ src/dom.js
@@ -2,12 +2,14 @@
 
 //Append projects to the dom
 const loadProjects = (projects) =>{
+    
     //Clear previous content first
-    //Create dom element for each project
-    //Attach event listeners
-    const container = document.querySelector('project-container');
+    const container = document.querySelector('.project-container');
     container.replaceChildren();
 
+    //Create dom element for each project
+    //(Not done yet) Attach event listeners
+    //Append created elements to the container    
     projects.forEach(project => {
         const singleProject = document.createElement('div');
         singleProject.classList.add('single-project');
@@ -31,10 +33,35 @@ const loadProjects = (projects) =>{
 }
 
 //Append tasks to the dom
-const loadTasks = () =>{
+const loadTasks = (tasks) =>{
     //Clear previous content first
+    const container = document.querySelector('.task-container');
+    container.replaceChildren();
     //Create dom element for each task
     //Attach event listeners
+    tasks.forEach(task => {
+        const taskCard = document.createElement('div');
+        taskCard.classList.add('task-card');
+        const priority = document.createElement('div');
+        priority.classList.add(`priority-${task.priority}`);
+        const taskTitle = document.createElement('div');
+        taskTitle.classList.add('task-title');
+        taskTitle.setAttribute('id', task.id);
+        taskTitle.textContent = task.title;
+        const editTask = document.createElement('div');
+        editTask.classList.add('edit-task');
+        editTask.setAttribute('id', task.id);
+        editTask.textContent = 'E'; //To be replaced with an edit icon
+        const deleteTask = document.createElement('div');
+        deleteTask.classList.add('delete-task');
+        deleteTask.setAttribute('id', task.id);
+        deleteTask.textContent = 'D'; //To be replaced with a delete icon
+        taskCard.appendChild(priority);
+        taskCard.appendChild(taskTitle);
+        taskCard.appendChild(editTask);
+        taskCard.appendChild(deleteTask);
+        container.appendChild(taskCard);
+    })   
 }
 
 //Create edit task form
@@ -48,3 +75,5 @@ const editProjectForm = (id) =>{
     //Pre-fill the form with data from project.id, then allow edits.
     //Attach event listeners
 }
+
+export {loadProjects, loadTasks, editTaskForm, editProjectForm}
--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
 import {addTask, editTask, removeTask, getTasks} from './tasks.js';
 import {addProject, removeProject, getProjects, editProject } from './projects.js';
 import {defaultDisplay, todayDisplay, projectDisplay, weekDisplay} from './display.js';
+import { loadProjects, loadTasks, editProjectForm, editTaskForm } from './dom.js';
 import './style.css';
 
 addProject('default');
 
-addTask('Task1', 'Desc', 'default', '2021-01-01', '1');
-addTask('Task2', 'Desc', 'default', '2021-04-02', '1');
-addTask('Task3', 'Desc', 'default', '2021-02-01', '1');
-addTask('Task4', 'Desc', 'default2', '2021-01-03', '1');
-addTask('Task5', 'Desc', 'default2', '2021-09-15', '1');
+addTask('Task1', 'Desc', 'default', '2021-01-01', 'high');
+addTask('Task2', 'Desc', 'default', '2021-04-02', 'medium');
+addTask('Task3', 'Desc', 'default', '2021-02-01', 'low');
+addTask('Task4', 'Desc', 'default2', '2021-01-03', 'low');
+addTask('Task5', 'Desc', 'default2', '2021-09-15', 'high');
+
+
+loadTasks(getTasks());
