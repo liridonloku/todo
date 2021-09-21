a1dd31a6c22600e362d10bf1df6aa8a7ffac99b7
Liridon Loku <liridonloku@gmail.com>
2021-09-21 13:01:36 +0200

    Add loadProjects function to dom.js

--- src/dom.js
+++ src/dom.js
@@ -6,7 +6,28 @@ const loadProjects = (projects) =>{
     //Create dom element for each project
     //Attach event listeners
     const container = document.querySelector('project-container');
-    
+    container.replaceChildren();
+
+    projects.forEach(project => {
+        const singleProject = document.createElement('div');
+        singleProject.classList.add('single-project');
+        const projectName = document.createElement('div');
+        projectName.classList.add('project-name');
+        projectName.setAttribute('id', project.id);
+        projectName.textContent = project.name;
+        const editProject = document.createElement('div');
+        editProject.classList.add('edit-project');
+        editProject.setAttribute('id', project.id);
+        editProject.textContent = 'E'; //To be replaced with an edit icon
+        const deleteProject = document.createElement('div');
+        deleteProject.classList.add('delete-project');
+        deleteProject.setAttribute('id', project.id);
+        deleteProject.textContent = 'D'; //To be replaced with a delete icon
+        singleProject.appendChild(projectName);
+        singleProject.appendChild(editProject);
+        singleProject.appendChild(deleteProject);
+        container.appendChild(singleProject);
+    })
 }
 
 //Append tasks to the dom
